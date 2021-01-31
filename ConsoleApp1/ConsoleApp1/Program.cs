57e20a5a50ecbaadb728f8f9ad9ac7396ab0816c
Alina <alinasap20@gmail.com>
2021-02-01 00:18:51 +0500

    project start

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = ?";
+            Console.WriteLine(a.Remove(2,14));
+        }
+    }
+}
