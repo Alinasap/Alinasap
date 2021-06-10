89508ada423e0ad606e005d93af04d69ec421532
Alinasap <71520289+Alinasap@users.noreply.github.com>
2021-06-10 06:16:00 +0500

    Update Project 5 (fin).cpp

--- Project 5 (fin).cpp
+++ Project 5 (fin).cpp
@@ -6,24 +6,39 @@ using namespace std;
 
 int main()
 {
-    int a;
-    int f = 1;
-    cin >> a;
-
-    __asm
+    setlocale(0, "");
+    int num;
+    cout << "Введите число:";
+    cin >> num;
+    if (num > 0) 
     {
-        mov ecx, 1 //счетчик цикла
-
-        mov eax, f //факториал
-
-        STEP1 :
-         mul ecx //eax * ecx
-         inc ecx //инкрементируем счетчик
-         cmp a, ecx
-            jae STEP1; // прыгаем, если ecx<=a
-         mov a, eax
+        _asm 
+        {
+            push num
+            push 1
+            call factorial //вызов процедуры
+            jmp end
+            factorial :
+               mov eax, [esp + 4] 
+               mov ebx, [esp + 8]
+               mul ebx //умножение без знака eax*ebx
+               dec ebx //-1
+               cmp ebx, 1
+               je exit //переход, если числа равны
+               push ebx
+               push eax
+               call factorial
+               pop ecx
+               pop ecx
+               ret
+            exit :
+               ret          //возвращает ближайшую процедуру
+            end :
+               mov num, eax
+               pop ecx
+               pop ecx
+        }
     }
-    cout << a << endl;
     return 0;
 }
 
